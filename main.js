5a3533c05cf518439a3212a8eaf0c9a7ab26099d
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-01-23 00:09:18 +0800

    disable old animations

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
 
 gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
 
-import "./src/services/text-animate";
+// import "./src/services/text-animate";
 import "./src/services/article-animate";
 import "./src/plugins/swiper";
 // import "./src/services/line-animate";
